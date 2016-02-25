6e82ab0609806369574143dd41ac16ad82d2ead2
dinomujki <dino_mujki@hotmail.com>
2016-02-25 13:43:05 -0600

    Removed buttons, works in background all-time

--- app/src/main/java/com/microsoft/band/sdk/heartrate/BandHeartRateAppActivity.java
+++ app/src/main/java/com/microsoft/band/sdk/heartrate/BandHeartRateAppActivity.java
@@ -90,8 +90,6 @@ public class BandHeartRateAppActivity extends YouTubeBaseActivity implements You
             if (event != null) {
                 bpm = event.getHeartRate();
                 appendTobpm(Integer.toString(bpm));
-            	//appendToUI(String.format("Heart Rate = %d beats per minute\n"
-                //        + "Quality = %s\n", event.getHeartRate(), event.getQuality()));
 
                 ParseObject testObject = new ParseObject("SensorData");
                 testObject.put("HeartRate", bpm);
@@ -171,29 +169,17 @@ public class BandHeartRateAppActivity extends YouTubeBaseActivity implements You
         txtrr = (TextView) findViewById(R.id.txtrr);
 
         txtgsr = (TextView) findViewById(R.id.txtgsr);
+        txtStatus.setText("");
+        new HeartRateSubscriptionTask().execute();
+        new GsrSubscriptionTask().execute();
+//        btnStart = (Button) findViewById(R.id.btnStart);
+//        btnStart.setOnClickListener(new OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//
+//			}
+//		});
 
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				txtStatus.setText("");
-				new HeartRateSubscriptionTask().execute();
-			}
-		});
-        
-        final WeakReference<Activity> reference = new WeakReference<Activity>(this);
-        
-        btnConsent = (Button) findViewById(R.id.btnConsent);
-        btnConsent.setOnClickListener(new OnClickListener() {
-			@SuppressWarnings("unchecked")
-            @Override
-			public void onClick(View v) {
-				new HeartRateConsentTask().execute(reference);
-                new GsrSubscriptionTask().execute();
-			}
-		});
-
-        //...here
     }
 
     public void setTheID(String myID) {
@@ -237,17 +223,17 @@ public class BandHeartRateAppActivity extends YouTubeBaseActivity implements You
 		txtStatus.setText("");
 	}
 	
-    @Override
-	protected void onPause() {
-		super.onPause();
-		if (client != null) {
-			try {
-				client.getSensorManager().unregisterHeartRateEventListener(mHeartRateEventListener);
-			} catch (BandIOException e) {
-				appendToUI(e.getMessage());
-			}
-		}
-	}
+//    @Override
+//	protected void onPause() {
+//		super.onPause();
+//		if (client != null) {
+//			try {
+//				client.getSensorManager().unregisterHeartRateEventListener(mHeartRateEventListener);
+//			} catch (BandIOException e) {
+//				appendToUI(e.getMessage());
+//			}
+//		}
+//	}
 	
     @Override
     protected void onDestroy() {
@@ -268,13 +254,8 @@ public class BandHeartRateAppActivity extends YouTubeBaseActivity implements You
 		protected Void doInBackground(Void... params) {
 			try {
 				if (getConnectedBandClient()) {
-					if (client.getSensorManager().getCurrentHeartRateConsent() == UserConsent.GRANTED) {
 						client.getSensorManager().registerHeartRateEventListener(mHeartRateEventListener);
                         client.getSensorManager().registerRRIntervalEventListener(mRREventListener);
-					} else {
-						appendToUI("You have not given this application consent to access heart rate data yet."
-								+ " Please press the Heart Rate Consent button.\n");
-					}
 				} else {
 					appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
 				}
@@ -336,45 +317,7 @@ public class BandHeartRateAppActivity extends YouTubeBaseActivity implements You
             return null;
         }
     }
-	
-	private class HeartRateConsentTask extends AsyncTask<WeakReference<Activity>, Void, Void> {
-		@Override
-		protected Void doInBackground(WeakReference<Activity>... params) {
-			try {
-				if (getConnectedBandClient()) {
-					
-					if (params[0].get() != null) {
-                        client.getSensorManager().requestHeartRateConsent(params[0].get(), new HeartRateConsentListener() {
-                            @Override
-                            public void userAccepted(boolean consentGiven) {
-							}
-					    });
-					}
-				} else {
-					appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
-				}
-			} catch (BandException e) {
-				String exceptionMessage="";
-				switch (e.getErrorType()) {
-				case UNSUPPORTED_SDK_VERSION_ERROR:
-					exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
-					break;
-				case SERVICE_ERROR:
-					exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
-					break;
-				default:
-					exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
-					break;
-				}
-				appendToUI(exceptionMessage);
 
-			} catch (Exception e) {
-				appendToUI(e.getMessage());
-			}
-			return null;
-		}
-	}
-	
 	private void appendToUI(final String string) {
 		this.runOnUiThread(new Runnable() {
             @Override
